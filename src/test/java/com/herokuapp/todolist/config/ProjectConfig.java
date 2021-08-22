42cd0d155c1145b2dbc92efc3f21556344ddedcf
Egor Muratov <egormuratov@yahoo.com>
2021-08-22 21:44:23 +0300

    added configs

---
+++ src/test/java/com/herokuapp/todolist/config/ProjectConfig.java
@@ -0,0 +1,37 @@
+package com.herokuapp.todolist.config;
+
+import org.aeonbits.owner.Config;
+
+@Config.LoadPolicy(Config.LoadType.MERGE)
+@Config.Sources({
+        "system:properties",
+        "classpath:config/local.properties",
+
+})
+public interface ProjectConfig extends Config {
+
+    @DefaultValue("chrome")
+    @Key("browser")
+    String browser();
+
+    @DefaultValue("91.0")
+    @Key("browserVersion")
+    String browserVersion();
+
+    @DefaultValue("https://api-nodejs-todolist.herokuapp.com")
+    @Key("baseUrl")
+    String baseUrl();
+
+    @DefaultValue("1920x1080")
+    @Key("browserSize")
+    String browserSize();
+
+    @Key("browserMobileView")
+    String browserMobileView();
+
+    @Key("remoteDriverUrl")
+    String remoteDriverUrl();
+
+    @Key("videoStorage")
+    String videoStorage();
+}
