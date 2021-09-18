b0180f0f7abaf3ad33d6d5c2bf969c44615867bf
egormuratov <egormuratov@yahoo.com>
2021-09-18 17:02:05 +0300

    Refactoring v2

--- src/test/java/com/herokuapp/todolist/config/Project.java
+++ src/test/java/com/herokuapp/todolist/config/Project.java
@@ -4,8 +4,4 @@ import org.aeonbits.owner.ConfigFactory;
 
 public class Project {
     public static ProjectConfig config = ConfigFactory.create(ProjectConfig.class, System.getProperties());
-
-    public static boolean isRemoteWebDriver() {
-        return !config.remoteDriverUrl().equals("");
-    }
 }
